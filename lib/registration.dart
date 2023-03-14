9cfa17076ea6fc1d9a560309386c0b5a3fe67f82
TulenDimacik <dimaoriginal@yandex.ru>
2023-03-14 10:58:11 +0300

    FireAuth

---
+++ lib/registration.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class Registration extends StatelessWidget {
+  const Registration({super.key});
+static const routeName = '/reg';
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
